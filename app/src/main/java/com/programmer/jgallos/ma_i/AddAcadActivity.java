b058eff87db1384aff037570908621eccbfcefa7
jgallos <hsitechnologies@gmail.com>
2019-02-07 15:25:22 +0800

    07/02/2019 3:25pm - add acad record

---
+++ app/src/main/java/com/programmer/jgallos/ma_i/AddAcadActivity.java
@@ -0,0 +1,176 @@
+package com.programmer.jgallos.ma_i;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class AddAcadActivity extends AppCompatActivity {
+
+    private ImageButton imageAcad;
+    private static final int GALLERY_REQUEST_CODE = 2;
+    private Uri uri = null;
+
+    private EditText editTitle;
+    private EditText editDesc;
+    private Button saveBtn;
+
+    private StorageReference storage;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseRef;
+    private DatabaseReference mDatabaseUsers;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+
+    private static final String TAG = AddAcadActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_acad);
+
+        imageAcad = (ImageButton)findViewById(R.id.imageAcad);
+        saveBtn = (Button)findViewById(R.id.saveBtn);
+        editDesc = (EditText)findViewById(R.id.editDesc);
+        editTitle = (EditText)findViewById(R.id.editTitle);
+        storage = FirebaseStorage.getInstance().getReference();
+        databaseRef = database.getInstance().getReference().child("Android_Development");
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
+
+        imageAcad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+
+                    galleryIntent.setType("image/*");
+                    startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+                } catch (Exception e) {
+                    Log.e(TAG,"exception", e);
+                }
+
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AddAcadActivity.this, "Saving...", Toast.LENGTH_SHORT).show();
+                final String recordTitle = editTitle.getText().toString().trim();
+                final String recordDesc = editDesc.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(recordTitle) && !TextUtils.isEmpty(recordDesc)){
+                    final StorageReference filepath = storage.child("Android_Development_images").child(uri.getLastPathSegment());
+
+                    UploadTask uploadTask = filepath.putFile(uri);
+
+                    Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                throw task.getException();
+                            }
+
+                            return filepath.getDownloadUrl();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                final Uri downloadUrl = task.getResult();
+                                Toast.makeText(AddAcadActivity.this, "Record saved.",Toast.LENGTH_SHORT).show();
+
+                                final DatabaseReference newAcad = databaseRef.push();
+                                mDatabaseUsers.addValueEventListener((new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        newAcad.child("title").setValue(recordTitle);
+                                        newAcad.child("desc").setValue(recordDesc);
+                                        newAcad.child("imageUrl").setValue(downloadUrl.toString());
+                                        newAcad.child("uid").setValue(mCurrentUser.getUid());
+                                        newAcad.child("username").setValue(dataSnapshot.child("name").getValue())
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Intent intent = new Intent(AddAcadActivity.this, ClassSessionActivity.class);
+                                                            startActivity(intent);
+                                                            finish();
+                                                        }
+                                                    }
+                                                });
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                }));
+
+                            }else {
+                                //handle failure
+                                Toast.makeText(AddAcadActivity.this, "Save failed.",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if (requestCode==GALLERY_REQUEST_CODE && resultCode==RESULT_OK) {
+            uri = data.getData();
+            imageAcad.setImageURI(uri);
+        }
+    }
+
+}
--- app/src/main/java/com/programmer/jgallos/ma_i/ClassSessionActivity.java
+++ app/src/main/java/com/programmer/jgallos/ma_i/ClassSessionActivity.java
@@ -1,17 +1,59 @@
 package com.programmer.jgallos.ma_i;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.view.MenuItem;
+
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.instacart.library.truetime.TrueTimeRx;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class ClassSessionActivity extends AppCompatActivity {
 
+    private EditText editMessage;
+    private Button signoutBtn;
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseRef;
+    private DatabaseReference mDatabaseUsers;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+
+    private String timeHolder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_session);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -25,6 +67,42 @@ public class ClassSessionActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //Spinner spinner = (Spinner) findViewById(R.id.spinnerLevel);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+       // ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.feedback_level45, android.R.layout.simple_spinner_item);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       // spinner.setAdapter(adapter);
+
+        signoutBtn = (Button)findViewById(R.id.signoutBtn);
+        editMessage = (EditText)findViewById(R.id.editMessage);
+
+        databaseRef = database.getInstance().getReference().child("Android Development_feedback");
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
+        signoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ClassSessionActivity.this, "Signing out...",Toast.LENGTH_SHORT).show();
+                final String feedback = editMessage.getText().toString().trim();
+                final DatabaseReference newFeedback = databaseRef.push();
+
+                newFeedback.child("feedbackLevel").setValue("5");
+                newFeedback.child("feedbackMessage").setValue(editMessage.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(ClassSessionActivity.this,MainActivity.class));
+                        }
+                    }
+                });
+
+
+            }
+        });
+
     }
 
     @Override
@@ -32,6 +110,43 @@ public class ClassSessionActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_session, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.action_addAcad) {
+            startActivity(new Intent(ClassSessionActivity.this, AddAcadActivity.class));
+        } else if (id==R.id.action_viewAcad) {
+          //  startActivity(new Intent(ClassSessionActivity.this, ViewAcadActivity.class));
+        } else if (id==R.id.action_viewAttendance) {
+          //  startActivity(new Intent(ClassSessionActivity.this, ViewAttendanceActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private String updateTime(int flag) {
+        TextView textTime = (TextView) findViewById(R.id.textViewOutTime);
+
+        if (!TrueTimeRx.isInitialized()) {
+            Toast.makeText(ClassSessionActivity.this, "TrueTime is not yet initialized.", Toast.LENGTH_SHORT).show();
+            return " ";
+        }
+
+        Date trueTime = TrueTimeRx.now();
+        if (flag == 1) {
+            textTime.setText(getString(R.string.tt_time_gmt, _formatDate(trueTime, "yyyy-MM-dd", TimeZone.getTimeZone("GMT+08:00"))));
+
+        } else if (flag==2) {
+            textTime.setText(getString(R.string.tt_time_gmt, _formatDate(trueTime, "HH:mm:ss", TimeZone.getTimeZone("GMT+08:00"))));
+        }
+        return textTime.getText().toString();
+    }
+
+    private String _formatDate(Date date, String pattern, TimeZone timeZone) {
+        DateFormat format = new SimpleDateFormat(pattern, Locale.ENGLISH);
+        format.setTimeZone(timeZone);
+        return format.format(date);
     }
 
 }
