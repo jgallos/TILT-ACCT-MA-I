1018272cb1066dace5bf1bca4d1bd4a9d4c9ba3a
jgallos <hsitechnologies@gmail.com>
2019-02-07 13:23:36 +0800

    07/02/2019 1:23pm - added login activity

--- app/src/main/java/com/programmer/jgallos/ma_i/LoginActivity.java
+++ app/src/main/java/com/programmer/jgallos/ma_i/LoginActivity.java
@@ -1,13 +1,36 @@
 package com.programmer.jgallos.ma_i;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private EditText loginEmail, loginPass;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private TextView signUpTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +39,70 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        signUpTextView = (TextView)findViewById(R.id.signUpTxtView);
+        loginBtn = (Button)findViewById(R.id.loginBtn);
+        loginEmail = (EditText)findViewById(R.id.login_email);
+        loginPass = (EditText)findViewById(R.id.login_password);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPass.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Processing...", Toast.LENGTH_SHORT).show();
+
+                    mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                checkUserExistence();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Can't login. User not found...", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Complete all fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    public void checkUserExistence() {
+
+        final String user_id = mAuth.getCurrentUser().getUid();
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild(user_id)) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "User not registered!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
